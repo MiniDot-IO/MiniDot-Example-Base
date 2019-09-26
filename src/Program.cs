3e8683d55945c4d6084040182576563c06f5c361
Morgan Lane <morganlane@bristowsutor.co.uk>
2019-09-26 15:45:58 +0100

    Basic .NET project configured

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicMiniDotBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am the basic MiniDot base project that can run MiniDot compiled code!");
+        }
+    }
+}
